b1d341256d9dc8801d6a4841a4b7fe04939c6ab7
QENTELLI\bhagyaraju.pati <bhagyaraju.pati@qentelli.com>
2020-11-29 11:48:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/test')
+def test():
+    my_dict = {"title": "Bhagi", "genre":"Thriller"}
+    return json.dumps(my_dict)
+
+if __name__ == "__main__":
+    print("Server is running")
+    app.run(debug=True)
